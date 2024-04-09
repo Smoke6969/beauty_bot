677747fc9c8411411a371e4e7934c835ea6f2f89
Oleksandr Smolka <o.smolka@astoundcommerce.com>
2024-04-09 11:20:31 +0300

    add gender selection

--- beauty_bot/booking_bot/management/commands/bot.py
+++ beauty_bot/booking_bot/management/commands/bot.py
@@ -1,39 +1,82 @@
-# booking_bot/management/commands/bot.py
 from django.core.management.base import BaseCommand
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Application, CommandHandler, ContextTypes, CallbackQueryHandler
 from django.conf import settings
 from booking_bot.models import Service, Specialist
+from asgiref.sync import sync_to_async
 
 
 class Command(BaseCommand):
-    help = 'Run the telegram bot'
 
-    def start(self, update: Update, context: CallbackContext):
-        update.message.reply_text('Hello, welcome to our beauty salon booking system. How can I assist you today?')
+    async def show_menu(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        keyboard = [
+            [InlineKeyboardButton("Послуги для чоловіків", callback_data="gender_men")],
+            [InlineKeyboardButton("послуги для жінок", callback_data="gender_women")]
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        await update.message.reply_text('Оберіть стать:', reply_markup=reply_markup)
 
-    def list_services(self, update: Update, context: CallbackContext):
-        services = Service.objects.all()
-        message = "Available Services:\n\n"
-        message += "\n".join([f"{service.name} - {service.duration} - ${service.price}" for service in services])
-        update.message.reply_text(message)
+    async def start(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        await self.show_menu(update, context)
 
-    def list_specialists(self, update: Update, context: CallbackContext):
-        specialists = Specialist.objects.all()
-        message = "Our Specialists:\n\n"
-        message += "\n".join([specialist.name for specialist in specialists])
-        update.message.reply_text(message)
+    async def list_services(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        services = await sync_to_async(list)(Service.objects.all())
+        keyboard = [
+            [InlineKeyboardButton(service.name, callback_data=f"service_{service.id}")] for service in services
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        await update.message.reply_text('Please choose a service:', reply_markup=reply_markup)
 
-    def add_handlers(self, dispatcher):
-        dispatcher.add_handler(CommandHandler('start', self.start))
-        dispatcher.add_handler(CommandHandler('services', self.list_services))
-        dispatcher.add_handler(CommandHandler('specialists', self.list_specialists))
+    async def list_specialists(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        specialists = await sync_to_async(list)(Specialist.objects.all())
+        keyboard = [
+            [InlineKeyboardButton(specialist.name, callback_data=f"specialist_{specialist.id}")] for specialist in
+            specialists
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        await update.message.reply_text('Please choose a specialist:', reply_markup=reply_markup)
 
-    def handle(self, *args, **kwargs):
-        updater = Updater(token=settings.TELEGRAM_BOT_TOKEN, use_context=True)
-        dispatcher = updater.dispatcher
+    async def callback_query_handler(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        query = update.callback_query
+        await query.answer()
+        data = query.data
 
-        self.add_handlers(dispatcher)
+        if data.startswith("gender_"):
+            context.user_data['gender'] = "men" if data == "gender_men" else "women"
+            await self.show_main_options(update, context, query.message.chat_id)
+        elif data.startswith("service_"):
+            service_id = data.split('_')[1]
+            service = await sync_to_async(Service.objects.get)(id=service_id)
+            await query.edit_message_text(text=f"Selected service: {service.name}")
+        elif data.startswith("specialist_"):
+            specialist_id = data.split('_')[1]
+            specialist = await sync_to_async(Specialist.objects.get)(id=specialist_id)
+            await query.edit_message_text(text=f"Selected specialist: {specialist.name}")
 
-        updater.start_polling()
-        updater.idle()
+    async def show_main_options(self, update: Update, context: ContextTypes.DEFAULT_TYPE, chat_id: int) -> None:
+        keyboard = [
+            [InlineKeyboardButton("Date and Time", callback_data="date_time")],
+            [InlineKeyboardButton("Services", callback_data="services")],
+            [InlineKeyboardButton("Specialists", callback_data="specialists")]
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        print(context.user_data['gender'])
+        await context.bot.send_message(chat_id=chat_id, text="Please select an option:", reply_markup=reply_markup)
+
+    async def services_men(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        context.user_data['gender'] = "men"
+        await self.show_main_options(update, context, update.message.chat_id)
+
+    async def services_women(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        context.user_data['gender'] = "women"
+        await self.show_main_options(update, context, update.message.chat_id)
+
+    def handle(self, *args, **kwargs) -> None:
+        application = Application.builder().token(settings.TELEGRAM_BOT_TOKEN).build()
+
+        application.add_handler(CommandHandler('services_man', self.services_men))
+        application.add_handler(CommandHandler('services_women', self.services_women))
+        application.add_handler(CommandHandler('start', self.start))
+        application.add_handler(CallbackQueryHandler(self.callback_query_handler))
+
+        application.run_polling()
